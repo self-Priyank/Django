081f164bf36ab87bc8b4e57149304d82bc027fc8
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-18 10:00:03 +0530

    1st view func created

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1 style='color:navy;'> Welcome to Django! <h1>")
+    return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
+    "<p style='color:lime; font-size:24px;'> This is app_1 </p></body>")
