f93b95829d700c32f0a503638b19fd1b99bb353e
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-19 13:44:55 +0530

    1st model created

--- intro/app_1/admin.py
+++ intro/app_1/admin.py
@@ -1,3 +1,4 @@
+from .models import Students
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Students)
--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    age = models.SmallIntegerField()
+    email = models.EmailField()
+    course = models.CharField(max_length=50)
