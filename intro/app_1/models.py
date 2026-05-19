ec806b4d0990d44069e1418ae66bb516b4090c36
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-19 13:50:51 +0530

    remove field

--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Students(models.Model):
-    id = models.SmallIntegerField(primary_key=True)
     name = models.CharField(max_length=20)
     age = models.SmallIntegerField()
     email = models.EmailField()
