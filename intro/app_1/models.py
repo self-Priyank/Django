f15c9325d26ad41b02dce5c81e246a5d0aa7c2c5
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-20 18:21:35 +0530

    No comment

--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, default='Name')
     age = models.SmallIntegerField()
     email = models.EmailField()
     city = models.CharField(max_length=50, default='City')
-    course = models.CharField(max_length=50)
+    course = models.CharField(max_length=50, default='course')
