c0a9c95266338178f91e4f37741cc6d71e414a66
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-20 10:06:38 +0530

    records added using panel

--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -4,4 +4,5 @@ class Student(models.Model):
     name = models.CharField(max_length=20)
     age = models.SmallIntegerField()
     email = models.EmailField()
+    city = models.CharField(max_length=50, default='Unknown')
     course = models.CharField(max_length=50)
