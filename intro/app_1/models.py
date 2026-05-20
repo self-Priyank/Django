5573125c2b0da3cf01b7c48d24cad0415d171389
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-20 19:28:45 +0530

    __str__() func implemented

--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(max_length=20, default='Name')
+    name = models.CharField(max_length=20)
     age = models.SmallIntegerField()
     email = models.EmailField()
     city = models.CharField(max_length=50, default='City')
     course = models.CharField(max_length=50, default='course')
+
+    def __str__(self):
+        return self.name
