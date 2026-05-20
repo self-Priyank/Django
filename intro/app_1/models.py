8b98094117f9dd68e3830a9dc54829c2da31e215
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-20 10:17:43 +0530

    No Comment!

--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -4,5 +4,5 @@ class Student(models.Model):
     name = models.CharField(max_length=20)
     age = models.SmallIntegerField()
     email = models.EmailField()
-    city = models.CharField(max_length=50, default='Unknown')
+    city = models.CharField(max_length=50, default='City')
     course = models.CharField(max_length=50)
