a5c06bde63c7c3aaa2dfb1adba573e1ae0640e75
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-22 00:24:50 +0530

    M, V and T connected

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -4,7 +4,9 @@ from . import models
 
 def home(request):
     return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
-    "<p style='color:lime; font-size:24px;'> This is app_1 </p></body>")
+    "<p style='color:deeppink; font-size:24px;'> This is app_1 </p></body>")
 
-def students():
-    return None
+def students(request):
+    all_students = models.Student.objects.all()
+    data = {'students': all_students}
+    return render(request, 'students.html', data)
