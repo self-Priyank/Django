95a8db475b5fbddc3e6a1ca47170686d430da159
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-21 19:25:52 +0530

    import models in views

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def home(request):
     return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
     "<p style='color:lime; font-size:24px;'> This is app_1 </p></body>")
+
+def students():
+    return None
