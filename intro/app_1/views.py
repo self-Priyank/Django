5c103823f7a1bb116e6b6f912906e39ba4452947
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-24 02:16:50 +0530

    No comment

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
 
-def home(request):
+def home():
     return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
     "<p style='color:deeppink; font-size:24px;'> This is app_1 </p></body>")
 
