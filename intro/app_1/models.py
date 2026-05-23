b05b7f4975d77222e925204a2d784ec8ee4962a2
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-23 20:08:55 +0530

    Validators implemented

--- intro/app_1/models.py
+++ intro/app_1/models.py
@@ -1,8 +1,9 @@
+from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(max_length=30)
-    age = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=30, validators=[MinLengthValidator(2)])
+    age = models.PositiveSmallIntegerField(validators=[MinValueValidator(15)])
     email = models.EmailField()
     city = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
