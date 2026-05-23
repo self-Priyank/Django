230618a64e509279844994252ae9714294970d1e
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-23 13:47:29 +0530

    MySQL installed in Django

--- intro/intro/settings.py
+++ intro/intro/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'intro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'MySQL',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
