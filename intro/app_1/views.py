5af16c7903531eba9b4c5f61e302addc3d53d366
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-26 00:14:17 +0530

    password for user 'Philippe' changed

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
 
-def home():
+def home(request):
     return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
     "<p style='color:deeppink; font-size:24px;'> This is app_1 </p></body>")
 
