8ca5115d0fe87687baa2606c1b7eba7fdad2c40d
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-29 12:02:21 +0530

    only active users

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -4,10 +4,16 @@ from django.http import HttpResponse
 from . import models
 
 def home(request):
-    return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
-    "<p style='color:deeppink; font-size:24px;'> This is app_1 </p></body>")
+    if request.user.is_active:
+        return HttpResponse("<body><h1 style='color:navy;'>Welcome to Django!</h1>" \
+        "<p style='color:deeppink; font-size:24px;'> This is app_1 </p></body>")
+    
+    return HttpResponse("Access denied")
 
 def students(request):
+    if not request.user.is_active:
+        return HttpResponse("Access denied")
+    
     all_students = models.Student.objects.all()
     data = {'students': all_students}
     return render(request, 'students.html', data)
