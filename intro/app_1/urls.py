2a646c4f35facc21948cfa2f0374fbb5c8c7d475
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-31 18:51:49 +0530

    logout_app func created

--- intro/app_1/urls.py
+++ intro/app_1/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.login_app, name='login'),
     path('students/', views.students, name='students'),
+    path('logout/', views.logout_app, name='logout'),
 ]
