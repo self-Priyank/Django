590e26ae09a0455d22363bf9d66963dd80f83e8a
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-05-31 15:02:28 +0530

    editing login_app func and login page

--- intro/app_1/views.py
+++ intro/app_1/views.py
@@ -8,17 +8,14 @@ def login_app(request):
         P = request.POST.get("password")
         user = authenticate(request, username=U, password=P)
         
-        if user is None:
-            return render(request, "app_1/login.html", {"error": "Invalid username or password"})    
+        if user is not None & user.is_active:
+            if user.username != U:
+                return render(request, "app_1/login.html", {"error": "Invalid username or password"})
+            else:  
+                login(request, user)
+                return redirect("students")    
         else:
-            if user.is_active:
-                if user.username != U:
-                    return render(request, "app_1/login.html", {"error": "Invalid username or password"})
-                else:  
-                    login(request, user)
-                    return redirect("students")
-            else:
-                return render(request, "app_1/login.html", {"error": "Invalid username or password"})   
+            return render(request, "app_1/login.html", {"error": "Invalid username or password"})   
     
     return render(request, "app_1/login.html")
 
