2ad612a3a7740161f118c5aadc1fe6117080aa79
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-06-13 23:14:42 +0530

    backend logic of GET /students/ created

--- intro/app_2/views.py
+++ intro/app_2/views.py
@@ -1,4 +1,14 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.shortcuts import render, redirect
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.shortcuts import redirect
 from app_1.models import Student
+
+class students(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_students = Student.objects.all()
+        return Response({"message": "No Comment!"})
