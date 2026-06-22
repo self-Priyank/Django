15f873d5e8b239263a4ef0f8dd561b0f316f2867
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-06-22 22:43:04 +0530

    No comment

--- intro/app_2/urls.py
+++ intro/app_2/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('student/', views.StudentView.as_view())
+    path('student/', views.StudentView.as_view(), name='student_api'),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
