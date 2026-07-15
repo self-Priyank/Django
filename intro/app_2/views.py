9a13ecd5e0d611d020bd8b12df41e347c214f640
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2026-07-15 22:07:25 +0530

    GenericAPIView implementation

--- intro/app_2/views.py
+++ intro/app_2/views.py
@@ -1,16 +1,21 @@
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.shortcuts import redirect
 from .serializer import StudentSerializer
 from app_1.models import Student
 
-class StudentView(APIView):
+class StudentView(GenericAPIView, ListModelMixin):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    def get(self, request):
-        all_students = Student.objects.all()
-        ser = StudentSerializer(all_students, many = True)
-        return Response(ser.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+#   def get(self, request):
+#       all_students = Student.objects.all()
+#       ser = StudentSerializer(all_students, many = True)
+#       return Response(ser.data)
